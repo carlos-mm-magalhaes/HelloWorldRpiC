102f4709801da78853e32bfde701981d5281fc3c
carlos-mm-magalhaes <carlos.marcone.magalhaes@gmail.com>
2022-03-16 18:37:56 +0100

    uncomment CMAKE_C_COMPILER

--- toolchain-rpi.cmake
+++ toolchain-rpi.cmake
@@ -4,4 +4,4 @@ SET(CMAKE_SYSTEM_VERSION 1)
 # Avoid running the target test program on the host (it would eventually be an error) 
 # set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
 # Define the cross compiler locations
-SET(CMAKE_C_COMPILER   arm-linux-gnueabi-gcc)
+# SET(CMAKE_C_COMPILER   arm-linux-gnueabi-gcc)
